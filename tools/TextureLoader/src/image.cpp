1630a38c84a9e48f931c8e6d9f065d767de2f0c5
Haro <longtao.chou@gmail.com>
2024-07-30 00:53:18 +0800

    更新texture loader

--- tools/TextureLoader/src/image.cpp
+++ tools/TextureLoader/src/image.cpp
@@ -22,6 +22,59 @@ void Image::encode(const EncodeInfo& encodeInfo, IDataBlob** dataBlob)
 
 IMAGE_FILE_FORMAT Image::GetFileFormat(const uint8_t* data, size_t dataSize, const char8_t* filePath)
 {
+    if(data != nullptr)
+    {
+        if(dataSize >= 4)
+        {
+            if(dataSize >= 8 && data[0] == 0x89 && data[1] == 0x50 && data[2] == 0x4E && data[3] == 0x47 
+                && data[4] == 0x0D && data[5] == 0x0A && data[6] == 0x1A && data[7] == 0x0A)
+            {
+                return IMAGE_FILE_FORMAT_PNG;
+            }
+            else if(dataSize >= 3 && data[0] == 0xFF && data[1] == 0xD8 && data[2] == 0xFF)
+            {
+                return IMAGE_FILE_FORMAT_JPEG;
+            }
+            else if(dataSize >=4 && (data[0] == 0x49 && data[1] == 0x20 && data[2] == 0x49 ) ||
+                (data[0] == 0x49 && data[1] == 0x49 && data[2] == 0x2A && data[3] == 0x00) ||
+                (data[0] == 0x4D && data[1] == 0x4D && data[2] == 0x00 && data[3] == 0x2A) ||
+                (data[0] == 0x4D && data[1] == 0x4D && data[2] == 0x00 && data[3] == 0x2B))
+            {
+                return IMAGE_FILE_FORMAT_TIFF;
+            }
+            else if(dataSize >= 2 && data[0] == 0x01 && data[1] == 0xDA)
+            {
+                return IMAGE_FILE_FORMAT_SGI;
+            }
+            else if(dataSize >=4 && data[0] == 0x44 && data[1] == 0x44 && data[2] == 0x53 && data[3] == 0x20)
+            {
+                return IMAGE_FILE_FORMAT_DDS;
+            }
+
+            static constexpr uint8_t KTX10_IDENTIFIER[12] = {0xAB, 0x4B, 0x54, 0x58, 0x20, 0x31, 0x31, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A};
+            static constexpr uint8_t KTX20_IDENTIFIER[12] = {0xAB, 0x4B, 0x54, 0x58, 0x20, 0x32, 0x30, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A};
+            if(dataSize >= 12 && (memcmp(data, KTX10_IDENTIFIER, sizeof(KTX10_IDENTIFIER)) == 0 || memcmp(data, KTX20_IDENTIFIER, 12) == 0))
+            {
+                return IMAGE_FILE_FORMAT_KTX;
+            }
+
+        }
+        else
+        {
+            return IMAGE_FILE_FORMAT_UNKNOWN;
+        }
+    }
+
+
+    if(filePath != nullptr)
+    {
+        const char* DotPos = strrchr((const char*)filePath, '.');
+        
+
+    }
+
+
+    return IMAGE_FILE_FORMAT_UNKNOWN;
 }
 
 Image::Image(const ImageDesc& desc, IDataBlob* dataBlob) : m_desc(desc), m_dataBlob(dataBlob)
